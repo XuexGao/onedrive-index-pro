c784992fbfefd76a046b3dac9139acd6bc5ba26a
XueGao <zihang518@outlook.com>
2026-05-17 11:15:36 +0800

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,11 +8,26 @@ class MyDocument extends Document {
         <Head>
           <meta name="description" content="OneDrive Index" />
           <link rel="icon" href="/favicon.ico" />
+
+          {/* LXGW WenKai Font */}
+          <link
+            rel="stylesheet"
+            href="https://npm.elemecdn.com/lxgw-wenkai-webfont@1.1.0/lxgwwenkai-regular.css"
+          />
+
+          {/* Original Google Fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           {siteConfig.googleFontLinks.map(link => (
             <link key={link} rel="stylesheet" href={link} />
           ))}
+
+          {/* Umami Analytics */}
+          <script
+            defer
+            src="https://u.xiegao.top/script.js"
+            data-website-id="20eb017f-1d2d-46c3-8369-ecbe9b53a87b"
+          />
         </Head>
         <body>
           <Main />
